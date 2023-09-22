0e7b1885bdb192d53e52e434eb8483dafb8b26b3
jisunahmad <100550065+jisunahmad@users.noreply.github.com>
2023-09-22 23:50:08 +0600

    Update insert at beginning.c

--- insert at beginning.c
+++ insert at beginning.c
@@ -7,35 +7,37 @@ struct node
 };
 void add_at_beginning(struct node **head,int data)
 {
-    struct node *ptr=*head;
+    struct node *ptr=*head; //put current head in the pointer ptr
     struct node *ptr2= (struct node*)malloc(sizeof(struct node));
-    ptr2->data=data;
-    ptr2->link=ptr;
-    *head=ptr2;
+    ptr2->data=data; //input data from the new node input
+    ptr2->link=ptr; //connect with the ptr (ptr is holding current head)
+    *head=ptr2; //assign ptr2 as head, the previous head is in pointer ptr now
 
 }
 int main()
 {
     int data;
     printf("Enter the data= ");
-    scanf("%d",&data);
+    scanf("%d",&data); //input the data for the new node
+
     struct node *head=(struct node*)malloc(sizeof(struct node));
     head->data=45;
     head->link=NULL;
 
-    struct node *head2=(struct node*)malloc(sizeof(struct node));
-    head2->data=98;
-    head2->link=NULL;
+    struct node *second=(struct node*)malloc(sizeof(struct node));
+    second->data=98;
+    second->link=NULL;
 
-    struct node *head3=(struct node*)malloc(sizeof(struct node));
-    head3->data=45;
-    head3->link=NULL;
+    struct node *third=(struct node*)malloc(sizeof(struct node));
+    third->data=45;
+    third->link=NULL;
 
-    head->link=head2;
-    head2->link=head3;
-    head3->link=NULL;
+    head->link=second;
+    second->link=third;
+    third->link=NULL;
+
     printf("Before inserting node at a  beginning\n");
-    printf("%d->%d->%d->NULL\n\n",head->data,head2->data,head3->data);
+    printf("%d->%d->%d->NULL\n\n",head->data,second->data,third->data);
 
     add_at_beginning(&head,data);
     printf("After inserting at beginnig\n");
